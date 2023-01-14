7600f229868de4f334484f09fd42a8842e0cef37
Josep <jcastellcolom@cifpfbmoll.eu>
2023-01-14 11:24:36 +0100

    feat: CardsCarousel (fetch musicians)

---
+++ src/components/CardsCarousel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import CardVue from './CardVue.vue';
+const data = ref(null);
+const error = ref(null);
+
+fetch('http://localhost:3000/musicians')
+  .then(res => res.json())
+  .then(json => (data.value = json))
+  .catch(err => (error.value = err));
+</script>
+
+<template>
+  <CardVue v-for="musician of data">
+    <template #name>{{ musician.name }}</template>
+    <template #description>{{ musician.description }}</template>
+  </CardVue>
+</template>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -12,10 +12,24 @@ export default {
   <header
     class="dark:bg-gray-800 w-full h-12 flex items-center text-gray-100 justify-between shadow-2xl fixed z-50"
   >
-    <h1 class="font-abril-fatface text-2xl ml-4 hover:scale-110 transition-transform duration-300 underline"><a href="/">MyMusician</a></h1>
+    <h1
+      class="font-abril-fatface text-2xl ml-4 hover:scale-110 transition-transform duration-300 underline"
+    >
+      <router-link to="/">MyMusician</router-link>
+    </h1>
     <div>
-      <a v-if="!userAuth()" class="text-xs hover:underline" href="/login">Log in</a>
-      <a v-if="!userAuth()" class="ml-2 mr-4 text-xs hover:underline" href="/register">Register</a>
+      <router-link
+        to="/login"
+        v-if="!userAuth()"
+        class="text-xs hover:underline"
+        >Log in</router-link
+      >
+      <router-link
+        v-if="!userAuth()"
+        class="ml-2 mr-4 text-xs hover:underline"
+        to="/register"
+        >Register</router-link
+      >
     </div>
   </header>
 </template>
