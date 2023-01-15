610f87b6f62532e8b85c06176383995122137c76
Josep <jcastellcolom@cifpfbmoll.eu>
2023-01-15 15:59:34 +0100

    feat: explore musicians link

--- src/components/CardsCarousel.vue
+++ src/components/CardsCarousel.vue
@@ -1,46 +1,59 @@
 <script lang="ts">
 export default {
-  data () {
+  data() {
     return {
       data: '',
       error: '',
       positionStart: true,
-
-    }
+    };
   },
   methods: {
     fetchData() {
       fetch('http://localhost:3000/musicians')
-      .then(res => res.json())
-      .then(json => (this.data = json))
-      .catch(err => (this.error = err));
+        .then(res => res.json())
+        .then(json => (this.data = json))
+        .catch(err => (this.error = err));
     },
     changePosition() {
       this.positionStart = !this.positionStart;
-    }
+    },
   },
   created() {
-    this.fetchData()
-  }
-}
-
+    this.fetchData();
+  },
+};
 </script>
 
 <template>
   <div v-if="!error" class="w-full relative">
-    <div v-if="!positionStart" @click="changePosition" class="border border-solid border-black hover:border-2 hover:border-b-0 hover:border-r-0 border-b-0 border-r-0 w-10 h-10 absolute left-0 top-1/2 -translate-y-1/2 -rotate-45"></div>
+    <div
+      v-if="!positionStart"
+      @click="changePosition"
+      class="border border-solid border-black hover:border-2 hover:border-b-0 hover:border-r-0 border-b-0 border-r-0 w-10 h-10 absolute left-52 top-1/2 -translate-y-1/2 -rotate-45"
+    ></div>
     <div class="flex w-[60rem] h-[34rem] mx-auto relative overflow-hidden">
-      <div class="w-[120rem] h-full flex items-center absolute top-0 transition-transform duration-1000" :class="{ left: positionStart, right: !positionStart}">
+      <div
+        class="w-[120rem] h-full flex items-center absolute top-0 transition-transform duration-1000"
+        :class="{ left: positionStart, right: !positionStart }"
+      >
         <card-vue v-for="musician in data" :key="musician.id">
-          <template #img><img :src="'src/assets/images/' + musician.imgName + '-sm.jpg'"/></template>
+          <template #img
+            ><img :src="'src/assets/images/' + musician.imgName + '-sm.jpg'"
+          /></template>
           <template #name>{{ musician.name }}</template>
           <template #description>{{ musician.description }}</template>
-          <template #instrument>Instrumento: {{ musician.instrument }}</template>
+          <template #instrument
+            >Instrumento: {{ musician.instrument }}</template
+          >
           <template #age>Edad: {{ musician.age }}</template>
         </card-vue>
       </div>
     </div>
-    <div v-if="positionStart" @click="changePosition" class="border border-solid border-black hover:border-2 hover:border-b-0 hover:border-l-0 border-b-0 border-l-0 w-10 h-10 absolute right-0 top-1/2 -translate-y-1/2 rotate-45"></div>
+    <div
+      v-if="positionStart"
+      @click="changePosition"
+      class="border border-solid border-black hover:border-2 hover:border-b-0 hover:border-l-0 border-b-0 border-l-0 w-10 h-10 absolute right-52 top-1/2 -translate-y-1/2 rotate-45"
+    ></div>
   </div>
   <div v-if="error">
     <p class="text-red-700">Error al cargar los datos</p>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,30 +10,42 @@ import CardsCarousel from '@/components/CardsCarousel.vue';
       <div
         class="absolute top-1/2 w-full -translate-y-1/2 backdrop-blur-sm bg-gradient-to-b from-black/0 via-black/30 to-black/0 py-12"
       >
-      <h3 class="text-7xl text-white drop-shadow-xl font-bold">Adopta un músico</h3>
-      <hr class="w-1/2 mx-auto mt-4" />
-      <p class="text-gray-200 drop-shadow-md mt-8 leading-7">
-        A parte de los famosos músicos ‘trending topic’, los demás pasan
-        hambre y frío en la calle.<br />
-        Nosotros te damos la posibilidad de adoptar una de estas criaturas
-        para que su vida sea un poco más fácil.<br />
-      </p>
-      <p class="text-gray-200 drop-shadow-md mt-4 leading-7">
-        Puedes elegir el que más te guste y mejor se adapte a tus
-        posibilidades económicas.</p>
-      <p class="text-gray-200 drop-shadow-md mt-7 leading-7">
-        ¡Él lo haría por tí!
-      </p>
-      <div class="w-px h-5 mt-4 mx-auto bg-white"></div>
-      <h3 class="font-abril-fatface italic text-2xl drop-shadow-lg text-gray-200 mt-4">MyMusician</h3>
+        <h3 class="text-7xl text-white drop-shadow-xl font-bold">
+          Adopta un músico
+        </h3>
+        <hr class="w-1/2 mx-auto mt-4" />
+        <p class="text-gray-200 drop-shadow-md mt-8 leading-7">
+          A parte de los famosos músicos ‘trending topic’, los demás pasan
+          hambre y frío en la calle.<br />
+          Nosotros te damos la posibilidad de adoptar una de estas criaturas
+          para que su vida sea un poco más fácil.<br />
+        </p>
+        <p class="text-gray-200 drop-shadow-md mt-4 leading-7">
+          Puedes elegir el que más te guste y mejor se adapte a tus
+          posibilidades económicas.
+        </p>
+        <p class="text-gray-200 drop-shadow-md mt-7 leading-7">
+          ¡Él lo haría por tí!
+        </p>
+        <div class="w-px h-5 mt-4 mx-auto bg-white"></div>
+        <h3
+          class="font-abril-fatface italic text-2xl drop-shadow-lg text-gray-200 mt-4"
+        >
+          MyMusician
+        </h3>
       </div>
     </div>
     <div
-      class="w-full h-screen relative z-10 bg-orange-100 flex justify-center items-center"
+      class="w-full h-screen relative z-10 bg-orange-100 flex flex-col justify-center items-center"
     >
       <div class="container w-screen flex justify-center items-center">
         <CardsCarousel />
       </div>
+      <button
+        class="mt-4 p-4 border-2 border-solid rounded-full text-white hover:drop-shadow-lg hover:scale-105 transition-transform duration-300 font-bold border-orange-800 bg-orange-600"
+      >
+        <route-link to="/explore">Encuentra más músicos</route-link>
+      </button>
     </div>
   </div>
 </template>
